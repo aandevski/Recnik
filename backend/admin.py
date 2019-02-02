dc6071d7ad557b6bd9a3e85bc4516c1d97a115d1
Aleksandar Andevski <a.andevski@gmail.com>
2019-02-02 18:38:47 +0100

    Add string repesentation to Word, register model in admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import Word
 
-admin.register(Word)
+admin.site.register(Word)
--- backend/models.py
+++ backend/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Word(models.Model):
 
-    word = models.TextField(max_length=100)
+    word = models.CharField(max_length=100)
     related_words = models.ManyToManyField("self")
     definition = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.word
