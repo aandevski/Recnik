f808b70eb831e1a266de041c4dc8b3cdac4b0856
Aleksandar Andevski <a.andevski@gmail.com>
2019-02-02 21:45:25 +0100

    Add react-router and react-router-dom

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const Footer = () => (
+    <footer>
+        <div className="container">
+            <p id="footerText">Footy footer</p>
+        </div>
+    </footer>
+);
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const Header = () => (
+    <header>
+        <section id="headerLeft">
+            <h1>Речник</h1>
+        </section>
+        <section id="headerRight">
+            <ul>
+                <li>Почетна</li>
+                <li>Речник</li>
+                <li>Калкулатор</li>
+                <li>Контакт</li>
+            </ul>
+        </section>
+    </header>
+);
